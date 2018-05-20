6c1427a9a29b1df2f641b74f2da10df459d1d38d
ram <ramchalla7684@gmail.com>
2018-05-20 22:24:37 +0530

    Uploading events documents

---
+++ events/js/event.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var register_button = document.getElementById('register');
+  if(register_button)
+  {
+    register_button.addEventListener('click', function(event) {
+    window.open(event.target.dataset.link, '_blank');
+  });
+}
+
+document.getElementById('hamburger_icon').addEventListener('click', openMenu);
+
+});
+
+
+function openMenu() {
+  var hamburger_line = document.getElementById('hamburger_line');
+  var list = document.getElementById('list');
+
+  var nav_wrapper = document.querySelector('#nav_wrapper');
+
+  if(hamburger_line.parentElement.classList.contains('hamburger_icon'))
+  {
+    //In open state
+    hamburger_line.classList.add('hamburger_open_line');
+    hamburger_line.parentElement.classList.remove('hamburger_icon');
+
+    nav_wrapper.style.display = "block";
+
+    setTimeout(function() {
+      list.style.display="block";
+    }, 250);
+
+  }
+  else
+  {
+    //In close state
+    hamburger_line.classList.remove('hamburger_open_line');
+    // menu.classList.remove('menu_open');
+    hamburger_line.parentElement.classList.add('hamburger_icon');
+
+    nav_wrapper.style.display = "none";
+    list.style.display="none";
+
+
+  }
+
+}
